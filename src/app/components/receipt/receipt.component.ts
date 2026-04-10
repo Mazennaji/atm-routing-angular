24b4f200deae382f6741f534cb6f6c76202f68a8
Mazen Naji <mazen.naji.1@gmail.com>
2026-04-10 15:45:56 +0300

    fix: remove duplicate Transaction interface from ReceiptComponent
    Transaction is already exported from atm.service.ts and imported
    on line 4. Duplicate local declaration caused TS2305 conflict.

--- src/app/components/receipt/receipt.component.ts
+++ src/app/components/receipt/receipt.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AtmService, Transaction } from '../../services/atm.service';
 
 @Component({
   selector: 'app-receipt',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, DatePipe],
   templateUrl: './receipt.component.html',
   styleUrls: ['./receipt.component.css']
 })
